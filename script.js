238227c073bca7c2ce7b847f6d1feb3cdd4082de
Ashley Stonebridge <ashley.ann.stonebridge@gmail.com>
2021-07-11 20:56:15 -0700

    question objects, question functions, button click events

--- script.js
+++ script.js
@@ -15,31 +15,35 @@ function setTime() {
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
+      // Calls function to display game over message
+      endQuiz();
     }
 
   }, 1000);
 }
 
 // Function to send game over message
-function sendMessage() {
+function endQuiz() {
   timeEl.textContent = " ";
-  gameOverEl.textContent = "Game Over";
+  gameOverEl.textContent = "End of Quiz";
 
 }
 
 var startGame = document.getElementById("start-button");
 
+var score = 0;
+
 function beginGame() {
+    score = 0;
     var startDisplay = document.getElementById("start");
     startDisplay.style.display = "none";
     setTime();
+    askQuestions();
 }
 
 startGame.addEventListener("click", beginGame);
 
-//store initials inputted and highscores via button increment
+//store initials inputted and highscores via button increment, display on screen
 var initials = document.getElementById("initialsSpan");
 var initialsText = document.getElementById("initialsText");
 var highscore = document.getElementById("highscoreSpan");
@@ -76,3 +80,71 @@ clearButton.addEventListener("click", function(){
   currentHighscore = "";
   localStorage.clear();
 });
+
+//question objects
+
+var questions = [
+  {
+    question: "What does CPA stand for?",
+    answers: ["A", "B", "Certified Public Accountant", "D"],
+    correctAnswer: "Certified Public Accountant"
+  },
+  {
+    question: "Who is Ashley?",
+    answers: ["Ashley is an accountant", "B", "C", "D"],
+    correctAnswer: "Ashley is an accountant"
+  }
+]
+
+var buttonA = document.getElementById("buttonA");
+var buttonB = document.getElementById("buttonB");
+var buttonC = document.getElementById("buttonC");
+var buttonD = document.getElementById("buttonD");
+var questionText = document.getElementById("questionText")
+var questionNumber = 0;
+
+function askQuestions() {
+  document.getElementById("correct").textContent = "";
+  document.getElementById("incorrect").textContent = "";
+  var question = questions[questionNumber];
+  questionText.textContent = question.question;
+  buttonA.textContent = question.answers[0];
+  buttonB.textContent = question.answers[1];
+  buttonC.textContent = question.answers[2];
+  buttonD.textContent = question.answers[3];
+}
+
+function nextQuestion() {
+  if (questionNumber < questions.length){
+    questionNumber++;
+    askQuestions();
+  }else{
+    endQuiz();
+  }
+}
+
+function buttonClicked(index){
+  var currentQuestion = questions[questionNumber];
+  if (currentQuestion.answers[index] === currentQuestion.correctAnswer) {
+    document.getElementById("correct").textContent = "Correct!";
+    score++;
+    document.getElementById("score").textContent = score;
+  }else{
+    document.getElementById("incorrect").textContent = "Incorrect!";
+    secondsLeft -= 5;
+  }
+  setTimeout(nextQuestion, 1000)
+}
+
+buttonA.addEventListener("click", function(){
+  buttonClicked(0);
+})
+buttonB.addEventListener("click", function(){
+  buttonClicked(1);
+})
+buttonC.addEventListener("click", function(){
+  buttonClicked(2);
+})
+buttonD.addEventListener("click", function(){
+  buttonClicked(3);
+})
